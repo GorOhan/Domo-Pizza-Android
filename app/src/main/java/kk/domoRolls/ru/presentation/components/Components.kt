894ab050e16da400dd9008385786ad36bb71151d
gorohanyan <gorwork21@gmail.com>
2024-08-19 15:33:06 +0400

    main screen optimization.

--- app/src/main/java/kk/domoRolls/ru/presentation/components/Components.kt
+++ app/src/main/java/kk/domoRolls/ru/presentation/components/Components.kt
@@ -1,6 +1,7 @@
 package kk.domoRolls.ru.presentation.components
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
@@ -46,12 +47,17 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.times
 import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Scale
+import coil.size.Size
+import coil.transform.Transformation
 import kk.domoRolls.ru.R
 import kk.domoRolls.ru.data.model.order.MenuItem
 import kk.domoRolls.ru.presentation.theme.DomoBorder
